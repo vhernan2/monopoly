a6ddd49600d2411fac3b1048a2282b506c02af82
Victor Hernandez <vhernan2@dhcp-10-24-180-184.dhcp.nd.edu>
2013-04-09 10:32:14 -0400

    Updating Log - Victor

---
+++ Victor/SDLMonopoly/SDLMonopoly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SDLMonopoly
+//
+//  Created by Victor Hernandez on 4/8/13.
+//  Copyright (c) 2013 Victor Hernandez. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Victor/SDLMonopoly/SDLMonopoly/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  SDLMonopoly
+//
+//  Created by Victor Hernandez on 4/8/13.
+//  Copyright (c) 2013 Victor Hernandez. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
