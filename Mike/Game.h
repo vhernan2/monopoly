246dddbceb2f11727be04033d3c2baeead12c7ec
m3han <mcreeha1@nd.edu>
2013-04-09 05:51:27 -0400

    Updated Files, Now with Houses and Hotels

---
+++ Mike/Game.h
@@ -0,0 +1,25 @@
+#ifndef GAME_H
+#define GAME_H
+#include "Player.h"
+#include "Tile.h"
+#include "Board.h"
+#include "Property.h"
+using namespace std;
+
+class Game
+{
+	public:
+		Game();
+		~Game();
+		void turn();
+		void playerTurn(Player*);
+		void rollDie(Player*);
+		void buildCheck(Player*);
+		void build(Player*);
+
+	private:
+		int numPlayers;
+		vector<Player> players;
+		Board gameBoard;
+};
+#endif
