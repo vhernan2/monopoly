3dab4991063f8c5dcb24e2076a2f31823b6162c3
vhernan2 <vehernan2@nd.edu>
2013-05-01 03:11:54 -0400

    updated main to test prompts

--- Victor/main.cpp
+++ Victor/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-#include "functions.h"
 #include "menu.h"
 #include "audio.h"
+#include "prompts.h"
 using namespace std;
 
 // global variables
@@ -11,6 +11,35 @@ using namespace std;
 int main (int argc, char* argv[]){
   SDL_Init(SDL_INIT_EVERYTHING);
   TTF_Init();
-  mainMenu();
+  font = TTF_OpenFont("/usr/share/fonts/sil-padauk/Padauk.ttf", 28);
+  if (TTF_Init() == -1 || font == NULL)
+    cout << "FONT ERROR" << endl;
+
+  SDL_Surface *test = TTF_RenderText_Shaded(font, "Name FOOBAR: ", textColor,bColor);
+  int players;
+  if(mainMenu() == -1)
+    players = startMenu();
+  
+  blit(10,10,test,display);
+  SDL_Flip(display);
+
+  string append(decipher(display));
+  string name;
+    
+  for (int i = 0 ; i < players ; i++){
+   
+    while (append != "0"){
+      name = name+append;
+      append = decipher(display);
+    }
+    cout << "Name: " << name << endl;
+    SDL_FillRect(display,NULL,0x000000);
+    SDL_Flip(display);
+
+    blit(10,10,test,display);
+    SDL_Flip(display);
+
+  }
+
 
 }
